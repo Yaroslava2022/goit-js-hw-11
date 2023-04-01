1f2f9f4755bebe685a0a700207c053fc3ab4328d
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-04-01 22:29:01 +0300

    5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import axios from "axios";
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const inputForm = document.querySelector('#search-form')
+const inputEl = document.querySelector('input[name="searchQuery"]')
+const galleryList = document.querySelector('.gallery')
+
+
+function fetchCountries() {
+    const image = inputEl.value.trim(); 
+   
+        return fetch(`https://pixabay.com/api/?key=24900959-71ad91e9c5a66d7f732acf0b4&q=cat&image_type=photo&orientation=horizontal&safesearch=true`).then(
+     (res) => {
+       if (!res.ok) {
+         throw new Error(res.status);
+       }
+       return res.json();
+     }
+     
+   );
+   }
+ 
+ 
+ function renderCountriesList(users) {
+     const markup = users
+       .map((user) => {
+         return `<div class="photo-card">
+         <img src=${user.hits.webformatURL} alt=${user.hits.tags} loading="lazy" />
+         <div class="info">
+           <p class="info-item">
+             <b>Likes:${user.hits.likes}</b>
+           </p>
+           <p class="info-item">
+             <b>Views: ${user.hits.views}</b>
+           </p>
+           <p class="info-item">
+             <b>Comments: ${user.hits.comments}</b>
+           </p>
+           <p class="info-item">
+             <b>Downloads: ${user.hits.downloads}</b>
+           </p>
+         </div>
+       </div>`
+ 
+       })
+       .join("");
+    galleryList.innerHTML = markup;
+    
+   }
+ 
+   inputEl.addEventListener("input", () => {
+   
+    fetchCountries( )
+      .then((users) =>
+        renderCountriesList(users) 
+)
+      .catch((error) => 
+      
+      Notiflix.Notify.failure('Oops, there is no country with that name'),
+      countryInfEl.innerHTML = "",
+      countryList.innerHTML = "",
+      );
+    });
+console.log(fetchCountries());
