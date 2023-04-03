32677c239e9a7013195a586ed118348e90d122c7
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-04-04 00:02:09 +0300

    simplelight

--- src/index.js
+++ src/index.js
@@ -11,9 +11,23 @@ const loadMoreBtnEl = document.querySelector('.load-more')
 const messageEL = document.querySelector('.message-end')
 let page = 1;
 const per_page = 40;
+let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250, });
 
 
 
+
+// const showBigPicture = () => {
+
+//    gallery.captionDelay = 250;
+//   gallery.on('show.simplelightbox');
+ 
+// };
+// const refreshBigPicture = () => {
+//   let gallery = new SimpleLightbox('.gallery a');
+  
+//   gallery.refresh();
+ 
+// };
 inputFormEl.addEventListener('submit', (e) => {
   e.preventDefault();
   page = 1;
@@ -26,6 +40,9 @@ inputFormEl.addEventListener('submit', (e) => {
       }
     
       renderImagesList(data);
+      
+      gallery.on('show.simplelightbox');
+      gallery.refresh();
       Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`)
       if (Math.ceil(data.totalHits / per_page) === page) {
       
@@ -44,9 +61,15 @@ inputFormEl.addEventListener('submit', (e) => {
 });
 function handleLoadMoreImages() {
   page += 1;
- fetchImages(page, per_page).then((data) => {
-   renderMoreImagesList(data)
-      if (Math.ceil(data.totalHits / per_page) === page) {
+ 
+  fetchImages(page, per_page).then((data) => {
+   
+    renderMoreImagesList(data);
+    
+ gallery.on('show.simplelightbox');
+ gallery.refresh();
+    
+          if (Math.ceil(data.totalHits / per_page) === page) {
         loadMoreBtnEl.classList.add('is-hidden');
          messageEL.classList.remove('is-hidden')
     } else {
@@ -59,5 +82,4 @@ function handleLoadMoreImages() {
   galleryList.textContent = 'no images'});
  
 }
-loadMoreBtnEl.addEventListener('click', handleLoadMoreImages )
-  //  
+loadMoreBtnEl.addEventListener('click', handleLoadMoreImages);
--- src/renderImagesList.js
+++ src/renderImagesList.js
@@ -2,10 +2,11 @@ export const galleryList = document.querySelector('.gallery');
 const logoPath = new URL('./images/icons.svg', import.meta.url);
 export const renderImagesList = (data) => {
   
-  const markup = (data.hits).map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+  const markup = (data.hits).map(({ webformatURL, tags, likes, views, comments, downloads, largeImageURL }) => {
     return `<div class="photo-card">
-        
+        <div class ="card-thumb"><a class="gallery__link" href="${largeImageURL}">
          <img src=${webformatURL} alt=${tags} loading="lazy" />
+         </div>
          <div class="info">
           <p class="info-item">
           <svg class="likes-icon" width="18" height="18">
@@ -36,22 +37,31 @@ export const renderImagesList = (data) => {
 
 export const renderMoreImagesList = (data) => {
   
-     const markup = (data.hits).map(({webformatURL, tags, likes, views, comments, downloads}) => {
+     const markup = (data.hits).map(({webformatURL, tags, likes, views, comments, downloads, largeImageURL}) => {
          return `<div class="photo-card">
-        
+        <div class ="card-thumb"><a class="gallery__link" href="${largeImageURL}">
          <img src=${webformatURL} alt=${tags} loading="lazy" />
+         </div>
          <div class="info">
+          <p class="info-item">
+          <svg class="likes-icon" width="18" height="18">
+      <use href="${logoPath}#icon-heart"></use>
+    </svg> ${likes}
+          </p>
            <p class="info-item">
-             <b>Likes: ${likes}</b>
+             <svg class="likes-icon" width="18" height="18">
+      <use href="${logoPath}#icon-eye"></use>
+    </svg> ${views}
            </p>
            <p class="info-item">
-             <b>Views: ${views}</b>
+             <svg class="likes-icon" width="18" height="18">
+      <use href="${logoPath}#icon-bubbles4"></use>
+    </svg> ${comments}
            </p>
            <p class="info-item">
-             <b>Comments: ${comments}</b>
-           </p>
-           <p class="info-item">
-             <b>Downloads: ${downloads}</b>
+             <svg class="likes-icon" width="18" height="18">
+      <use href="${logoPath}#icon-download"></use>
+    </svg> ${downloads}
            </p>
          </div>
 </div>`
